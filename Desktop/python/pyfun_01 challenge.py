a9424817f8dda6e53d9ddbdbc3620c3ef51f48c0
@eric <ericuwizeyimana24@gmail.com>
2023-11-01 18:59:08 +0200

    string shuffle

--- Desktop/python/pyfun_01 challenge.py
+++ Desktop/python/pyfun_01 challenge.py
@@ -1,5 +1,3 @@
-print("welcome to python")
-num1=20
-num2=15
-result=num1+num2
-print(f"the sum of {num1} and {num2} is {result}")
+original_string="python extensions "
+reversed_string=print(original_string[::-1])
+print(reversed_string)
